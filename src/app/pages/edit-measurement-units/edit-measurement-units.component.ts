27ae9d430ea0340d070ffe36fb5b8a6933ccd83b
DarkMage17 <josue.c1012@gmail.com>
2024-04-23 19:59:15 -0500

    chore: added validation in edit measurement

--- src/app/pages/edit-measurement-units/edit-measurement-units.component.ts
+++ src/app/pages/edit-measurement-units/edit-measurement-units.component.ts
@@ -1,34 +1,45 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CreateMeasurementUnit } from 'src/app/models/CreateMeasurementUnit';
 import { AuthService } from 'src/app/services/auth.service';
 import { MeasurementUnitService } from 'src/app/services/measurement-unit.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-edit-measurement-units',
   templateUrl: './edit-measurement-units.component.html',
-  styleUrls: ['./edit-measurement-units.component.scss']
+  styleUrls: ['./edit-measurement-units.component.scss'],
 })
-export class EditMeasurementUnitsComponent implements OnInit{
-  editMeasurementUnit: CreateMeasurementUnit = new CreateMeasurementUnit(); // Consider renaming CreateProduct to Product for better clarity
+export class EditMeasurementUnitsComponent implements OnInit {
+  editMeasurementUnit: CreateMeasurementUnit = new CreateMeasurementUnit();
   measurementUnitId: number | null = null;
   loggedUser: any;
+  form: FormGroup;
 
   constructor(
     private authService: AuthService,
     private route: ActivatedRoute,
     private router: Router,
     private measurementService: MeasurementUnitService,
-    private cdr: ChangeDetectorRef
-  ) {}
+    private formBuilder: FormBuilder
+  ) {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
+      abbreviation: [
+        '',
+        [Validators.required, Validators.pattern('[a-zA-Z. ]*')],
+      ],
+    });
+  }
 
   ngOnInit(): void {
-    this.authService.currentUser.subscribe(x => {
-      this.loggedUser = x;
+    this.authService.currentUser.subscribe((user) => {
+      this.loggedUser = user;
       if (!this.loggedUser || Object.keys(this.loggedUser).length === 0) {
         this.router.navigate(['login']);
       } else {
-        this.measurementUnitId = this.route.snapshot.paramMap.get('id') as number | null;
+        this.measurementUnitId = Number(this.route.snapshot.paramMap.get('id'));
         if (this.measurementUnitId) {
           this.loadMeasurementUnitDetails(this.measurementUnitId);
         }
@@ -37,20 +48,53 @@ export class EditMeasurementUnitsComponent implements OnInit{
   }
 
   loadMeasurementUnitDetails(measurementUnitId: number): void {
-    this.measurementService.getMeasurementUnitById(measurementUnitId).subscribe(
-      measurementUnit => {
+    this.measurementService
+      .getMeasurementUnitById(measurementUnitId)
+      .subscribe((measurementUnit) => {
         this.editMeasurementUnit = measurementUnit;
-        console.log(this.editMeasurementUnit);
-        this.cdr.detectChanges();
-      }
-    );
+        this.form.patchValue({
+          name: this.editMeasurementUnit.name,
+          abbreviation: this.editMeasurementUnit.abbreviation,
+        });
+      });
   }
 
   updateMeasurementUnit(): void {
-    this.measurementService.updateMeasurementUnit(this.measurementUnitId!, this.editMeasurementUnit).subscribe(
-      response => {
-        this.router.navigate(['measurement-units']);
-      }
+    if (this.form.valid) {
+      this.editMeasurementUnit = {
+        ...this.editMeasurementUnit,
+        ...this.form.value,
+      };
+      this.measurementService
+        .updateMeasurementUnit(
+          this.measurementUnitId!,
+          this.editMeasurementUnit
+        )
+        .subscribe((response) => {
+          this.router.navigate(['measurement-units']);
+        });
+    }
+  }
+
+  onSubmit(): void {
+    if (this.form.invalid) {
+      this.sendRequired();
+    } else {
+      this.editMeasurementUnit = this.form.value;
+      this.updateMeasurementUnit();
+      this.sendSuccess();
+    }
+  }
+
+  sendRequired(): void {
+    Swal.fire('Error', 'Resuelva los errores en el formulario', 'error');
+  }
+
+  sendSuccess(): void {
+    Swal.fire(
+      'Edicion exitosa',
+      'La unidad de medida se editó exitosamente',
+      'success'
     );
   }
 
